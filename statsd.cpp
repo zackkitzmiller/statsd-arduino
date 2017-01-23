6d84a1ab89f3f3fc1c877de5536d7d1ef93557e3
zackkitzmiller <zackkitzmiller@gmail.com>
2017-01-23 12:19:26 -0500

    formatting fixes

--- statsd.cpp
+++ statsd.cpp
@@ -7,43 +7,43 @@
 
 EthernetUDP statsd;
 
-StatsD::StatsD(IPAddress ip, int port, int localPort){
+StatsD::StatsD(IPAddress ip, int port, int localPort) {
   _ip = ip;
   _port = port;
   statsd.begin(localPort);
 }
 
 void StatsD::increment(const char *metric) {
-  StatsD::_send(metric,1,"c");
+  StatsD::_send(metric,1, "c");
 }
 
 void StatsD::decrement(const char *metric) {
-  StatsD::_send(metric,-1,"c");
+  StatsD::_send(metric,-1, "c");
 }
 
 void StatsD::timing(const char *metric, int ms) {
   StatsD::_send(metric,ms, "ms");
 }
 
-void StatsD::gauge(const char *metric, int gaugeValue){
-  StatsD::_send(metric,gaugeValue,"g");
+void StatsD::gauge(const char *metric, int gaugeValue) {
+  StatsD::_send(metric,gaugeValue, "g");
 }
 
-void StatsD::sets(const char *metric, int setsValue){
+void StatsD::sets(const char *metric, int setsValue) {
   StatsD::_send(metric, setsValue, "s");
 }
 
 void StatsD::_send(const char *metric, int value, const char *cmd) {
-  //Convert the integer to a string (an int's maximum string length is 12 characters [sign(1) + digits(10) + null(1)]) –2,147,483,648 to 2,147,483,647
+  // Convert the integer to a string (an int's maximum string length is 12 characters [sign(1) + digits(10) + null(1)]) ï¿½2,147,483,648 to 2,147,483,647
   char valueString[12];
   itoa(value,valueString,10);
 
-  //Concatenate the parts of the final string
-  char buffer[strlen(metric) + strlen(valueString) + strlen(cmd) + 2 + 1]; //+2 for : and | and add +1 for null
+  // Concatenate the parts of the final string
+  char buffer[strlen(metric) + strlen(valueString) + strlen(cmd) + 2 + 1]; // +2 for : and | and add +1 for null
   strcpy(buffer, metric);
-  strcat(buffer,":");
-  strcat(buffer,valueString);
-  strcat(buffer,"|");
+  strcat(buffer, ":");
+  strcat(buffer, valueString);
+  strcat(buffer, "|");
   strcat(buffer, cmd);
 
   const uint8_t* msg = (uint8_t*)buffer;
@@ -52,4 +52,3 @@ void StatsD::_send(const char *metric, int value, const char *cmd) {
   statsd.write(msg, strlen(buffer));
   statsd.endPacket();
 }
-
